3afe86509296c64be4d86ffe611eb08d1fbdc6d2
guipedrassa <guipedrassa@gmail.com>
2016-04-05 11:25:03 +0200

    remove old java doc

--- AManager/src/main/java/com/amanager/repository/AnimalRepository.java
+++ AManager/src/main/java/com/amanager/repository/AnimalRepository.java
@@ -9,10 +9,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.amanager.entity.Animal;
 
-/**
- * @author geci
- *
- */
 public interface AnimalRepository extends MongoRepository<Animal, String> {
 
 	public Animal findByFirstName(String firstName);
--- AManager/src/main/java/com/amanager/repository/GreetingRepository.java
+++ AManager/src/main/java/com/amanager/repository/GreetingRepository.java
@@ -7,10 +7,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.amanager.entity.Greeting;
 
-/**
- * @author geci
- *
- */
+
 public interface GreetingRepository extends MongoRepository<Greeting, String> {
 
 	public Greeting findByContent(String content);
